5a9a8dbbd11c5f09385966fd138d55dd463b3ca4
Severin Gehwolf <sgehwolf@redhat.com>
2019-10-30 11:25:48 +0100

    Prepare for 11.0.6 EA builds cycle
    - Add EA designator
    - Use 11.0.5+10 GA release as boot JDK

--- install-rhel-deps-build-openjdk11.sh
+++ install-rhel-deps-build-openjdk11.sh
@@ -62,11 +62,11 @@ EOF
 # Originally boot-strapped with build-openjdk9.sh and build-openjdk10.sh
 # For simplicity download a suitable boot JDK from AdoptOpenJDK.
 pushd /opt
-wget -O jdk-11.0.4_11.tar.gz "https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.4%2B11/OpenJDK11U-jdk_$(platform_name)_11.0.4_11.tar.gz"
-tar -xf jdk-11.0.4_11.tar.gz
-/opt/openjdk-11.0.4+11/bin/java -version
+wget -O jdk-11.0.5_10.tar.gz "https://github.com/AdoptOpenJDK/openjdk11-upstream-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jdk_$(platform_name)_11.0.5_10.tar.gz"
+tar -xf jdk-11.0.5_10.tar.gz
+/opt/openjdk-11.0.5+10/bin/java -version
 popd
-BOOT_JDK="/opt/openjdk-11.0.4+11/"
+BOOT_JDK="/opt/openjdk-11.0.5+10/"
 
 yum -y install $(echo $(cat $BRS_FILE))
 
@@ -105,13 +105,13 @@ cat > $BUILD_SCRIPT <<EOF
 #!/bin/bash
 set -e
 
-UPDATE="11.0.5"
-BUILD=10
+UPDATE="11.0.6"
+BUILD=1
 NAME="openjdk-\${UPDATE}+\${BUILD}"
 JRE_NAME="\${NAME}-jre"
 TEST_IMAGE_NAME="\${NAME}-test-image"
 TARBALL_BASE_NAME="OpenJDK11U"
-EA_SUFFIX=""
+EA_SUFFIX="_ea"
 PLATFORM="$(platform_name)"
 TARBALL_VERSION="\${UPDATE}_\${BUILD}\${EA_SUFFIX}"
 PLATFORM_VERSION="\${PLATFORM}_\${TARBALL_VERSION}"
